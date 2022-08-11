601b67f29cf29b9e4fd3149d74024f9ec5eb35bf
Matej Chlustik <matejchlustik221@gmail.com>
2022-08-11 15:12:04 +0200

    Fifth attempt at deploy

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.use(authRoutes);
 
 //serve frontend
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/build")))
+    app.use(express.static(path.join(__dirname, "build")))
 
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "/build", "index.html")))
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")))
 } else {
     app.get("/", (req, res) => {
         res.send("Please set to production")
